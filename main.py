a4a59ddbfeb5df44a8d18cf9d91699d2bd322e8a
f0xit <m.loe@gmx.de>
2023-02-03 15:52:27 +0100

    refactor of the main file every discord related thing is now inside its corresponding class

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+import asyncio
 import os
 import sys
 import getopt
 import logging
 import subprocess
-import random
-import re
 import datetime as dt
 from dotenv import load_dotenv
 import discord
-from discord.ext import commands, tasks
-from event import Event
+from discord.ext import commands
 from myfunc import strfdelta
 from bot import Bot
 from tools.logger_tools import LoggerTools
@@ -39,8 +37,6 @@ if discord_token is None:
 else:
     logging.info('Discord token loaded successfully.')
 
-moevius = Bot()
-
 
 class Administration(commands.Cog, name='Administration'):
     '''Diese Kategorie erfordert bestimmte Berechtigungen'''
@@ -48,7 +44,48 @@ class Administration(commands.Cog, name='Administration'):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
-    @is_super_user()
+    async def load_ext(self, ctx: commands.Context, extension: str) -> None:
+        '''Tries to load the given extension.
+
+        Args:
+            ctx (commands.Context): Invocation context. Needed for feedback message.
+            extension (str): Name of the extension located in the cogs directory.
+        '''
+        try:
+            await self.bot.load_extension(f'cogs.{extension}')
+            await ctx.send('Die Extension wurde geladen.')
+            logging.info('Extension loaded: %s', extension)
+        except commands.ExtensionNotFound:
+            await ctx.send(f'Fehler: Extension konnte nicht gefunden werden!')
+            logging.warning('Extension not existing: %s', extension)
+        except commands.ExtensionAlreadyLoaded:
+            await ctx.send(f'Fehler: Extension bereits geladen!')
+            logging.warning('Extension already loaded: %s', extension)
+        except commands.NoEntryPointError:
+            await ctx.send(f'Fehler: Extension hat keine Setup-Funktion!')
+            logging.warning('Extension has no setup function: %s', extension)
+        except commands.ExtensionFailed:
+            await ctx.send(f'Fehler: Extension Setup fehlgeschlagen!')
+            logging.warning('Extension setup failed: %s', extension)
+
+    async def unload_ext(self, ctx: commands.Context, extension: str) -> None:
+        '''Tries to unload the given extension.
+
+        Args:
+            ctx (commands.Context): Invocation context. Needed for feedback message.
+            extension (str): Name of the extension located in the cogs directory.
+        '''
+        try:
+            await self.bot.unload_extension(f'cogs.{extension}')
+            await ctx.send('Die Extension wurde entfernt.')
+            logging.info('Extension unloaded: %s', extension)
+        except commands.ExtensionNotFound:
+            await ctx.send(f'Fehler: Extension konnte nicht gefunden werden!')
+            logging.warning('Extension not existing: %s', extension)
+        except commands.ExtensionNotLoaded:
+            await ctx.send(f'Fehler: Extension nicht geladen!')
+            logging.warning('Extension not loaded: %s', extension)
+
     @commands.group(
         name='bot',
         aliases=['b'],
@@ -102,49 +139,23 @@ class Administration(commands.Cog, name='Administration'):
             STARTUP_TIME.strftime('%Y.%m.%d %H:%M:%S')
         )
 
+    @is_super_user()
     @_bot.command(
         name='reload',
         aliases=['-r']
     )
     async def _reload(self, ctx: commands.Context) -> None:
+        if ctx.prefix == '?':
+            return
+
         logging.warning('%s hat einen Reload gestartet.', ctx.author.name)
         await ctx.send('Reload wird gestartet.')
 
         self.bot.load_files_into_attrs()
         self.bot.analyze_guild()
 
-    async def load_ext(self, ctx: commands.Context, extension: str) -> None:
-        try:
-            await self.bot.load_extension(f'cogs.{extension}')
-            await ctx.send('Die Extension wurde geladen.')
-            logging.info('Extension loaded: %s', extension)
-        except commands.ExtensionNotFound:
-            await ctx.send(f'Fehler: Extension konnte nicht gefunden werden!')
-            logging.warning('Extension not existing: %s', extension)
-        except commands.ExtensionAlreadyLoaded:
-            await ctx.send(f'Fehler: Extension bereits geladen!')
-            logging.warning('Extension already loaded: %s', extension)
-        except commands.NoEntryPointError:
-            await ctx.send(f'Fehler: Extension hat keine Setup-Funktion!')
-            logging.warning('Extension has no setup function: %s', extension)
-        except commands.ExtensionFailed:
-            await ctx.send(f'Fehler: Extension Setup fehlgeschlagen!')
-            logging.warning('Extension setup failed: %s', extension)
-
-    async def unload_ext(self, ctx: commands.Context, extension: str) -> None:
-        try:
-            await self.bot.unload_extension(f'cogs.{extension}')
-            await ctx.send('Die Extension wurde entfernt.')
-            logging.info('Extension unloaded: %s', extension)
-        except commands.ExtensionNotFound:
-            await ctx.send(f'Fehler: Extension konnte nicht gefunden werden!')
-            logging.warning('Extension not existing: %s', extension)
-        except commands.ExtensionNotLoaded:
-            await ctx.send(f'Fehler: Extension nicht geladen!')
-            logging.warning('Extension not loaded: %s', extension)
-
-    @ is_super_user()
-    @ commands.group(
+    @is_super_user()
+    @commands.group(
         name='extensions',
         aliases=['ext'],
         brief='Verwaltet die Extensions des Bots.'
@@ -163,100 +174,94 @@ class Administration(commands.Cog, name='Administration'):
             ', '.join(self.bot.extensions.keys())
         )
 
-    @ _extensions.command(
+    @is_super_user()
+    @_extensions.command(
         name='load',
         aliases=['-l'],
         brief='Lädt eine Extension in den Bot.'
     )
     async def _load(self, ctx: commands.Context, extension: str) -> None:
+        if ctx.prefix == '?':
+            return
+
         await self.load_ext(ctx, extension)
 
-    @ _extensions.command(
+    @is_super_user()
+    @_extensions.command(
         name='unload',
         aliases=['-u'],
         brief='Entfernt eine Extension aus dem Bot.'
     )
     async def _unload(self, ctx: commands.Context, extension: str) -> None:
+        if ctx.prefix == '?':
+            return
+
         await self.unload_ext(ctx, extension)
 
-    @ _extensions.command(
+    @is_super_user()
+    @_extensions.command(
         name='reload',
         aliases=['-r'],
         brief='Lädt eine Extension neu.'
     )
     async def _reload(self, ctx: commands.Context, extension: str) -> None:
+        if ctx.prefix == '?':
+            return
+
         await self.unload_ext(ctx, extension)
         await self.load_ext(ctx, extension)
 
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:
+        self.bot.analyze_guild()
 
-@ moevius.event
-async def on_message(message: discord.Message) -> None:
-    if message.author == moevius.user:
-        return
+        for filename in os.listdir('./cogs'):
+            if (filename.endswith('.py')
+                    and not filename.startswith('__')
+                    and f"cogs.{filename[:-3]}" not in self.bot.extensions.keys()):
+                await self.bot.load_extension(f"cogs.{filename[:-3]}")
 
-    # Requests from file
-    if message.content[1:] in moevius.responses['req'].keys():
-        response = moevius.responses['req'][message.content[1:]]
-        for res in response['res']:
-            await message.channel.send(res.format(**locals(), **globals()))
-        logging.info(response['log'].format(**locals(), **globals()))
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author == self.bot.user:
+            return
 
-    # Responses from file
-    else:
-        for key in moevius.responses['res'].keys():
-            if re.search(key, message.content):
-                response = moevius.responses['res'][key]
-                for res in response['res']:
-                    await message.channel.send(
-                        content=res.format(**locals(), **globals()), tts=False
-                    )
-                logging.info(response['log'].format(**locals(), **globals()))
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        logging.error(
+            "%s - %s - %s",
+            ctx.author.name, ctx.message.content, error
+        )
+        await self.bot.get_user(247117682875432960).send(
+            f"```*ERROR*\n{ctx.author.name}\n{ctx.message.content}\n{error}```"
+        )
 
-    await moevius.process_commands(message)
+    @commands.Cog.listener()
+    async def on_guild_channel_create(self, channel: discord.abc.GuildChannel) -> None:
+        logging.info(
+            'New channel created: [ID:%s] %s', channel.id, channel.name
+        )
+        self.bot.analyze_guild()
 
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, channel: discord.abc.GuildChannel) -> None:
+        logging.info('Channel deleted: [ID:%s] %s', channel.id, channel.name)
+        self.bot.analyze_guild()
 
-@ moevius.event
-async def on_ready() -> None:
-    moevius.analyze_guild()
-
-    for filename in os.listdir('./cogs'):
-        if (filename.endswith('.py')
-                and not filename.startswith('__')
-                and f"cogs.{filename[:-3]}" not in moevius.extensions.keys()):
-            await moevius.load_extension(f"cogs.{filename[:-3]}")
-
-    await moevius.add_cog(Administration(moevius))
-
-
-@ moevius.event
-async def on_command_error(ctx: commands.Context, error: commands.CommandError):
-    logging.error("%s - %s - %s", ctx.author.name, ctx.message.content, error)
-    await moevius.get_user(247117682875432960).send(
-        f"```*ERROR*\n{ctx.author.name}\n{ctx.message.content}\n{error}```"
-    )
-
-
-@ moevius.event
-async def on_guild_channel_create(channel: discord.abc.GuildChannel) -> None:
-    logging.info('New channel created: [ID:%s] %s', channel.id, channel.name)
-    moevius.analyze_guild()
-
-
-@ moevius.event
-async def on_guild_channel_delete(channel: discord.abc.GuildChannel) -> None:
-    logging.info('Channel deleted: [ID:%s] %s', channel.id, channel.name)
-    moevius.analyze_guild()
-
-
-@ moevius.event
-async def on_guild_channel_update(
-    before: discord.abc.GuildChannel,
-    after: discord.abc.GuildChannel
-) -> None:
-    logging.info('Channel updated: [ID:%s] %s > %s',
-                 after.id, before.name, after.name)
-    moevius.analyze_guild()
+    @commands.Cog.listener()
+    async def on_guild_channel_update(
+        self,
+        before: discord.abc.GuildChannel,
+        after: discord.abc.GuildChannel
+    ) -> None:
+        logging.info(
+            'Channel updated: [ID:%s] %s > %s',
+            after.id, before.name, after.name
+        )
+        self.bot.analyze_guild()
 
 
 if __name__ == "__main__":
+    moevius = Bot()
+    asyncio.run(moevius.add_cog(Administration(moevius)))
     moevius.run(discord_token)
