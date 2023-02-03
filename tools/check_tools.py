9f46f1e1e4148f28b63ae47d5103a764b2982d0a
f0xit <m.loe@gmx.de>
2023-02-03 15:01:12 +0100

    small change in check_tools request commands should be independent from sper-user-rank

--- tools/check_tools.py
+++ tools/check_tools.py
@@ -5,5 +5,6 @@ from tools import json_tools
 def is_super_user():
     async def wrapper(ctx: commands.Context):
         settings = json_tools.load_file('json/settings.json')
-        return ctx.author.name in settings['super-users']
+        # Request commands should be independent from su-rank
+        return ctx.prefix == '?' or ctx.author.name in settings['super-users']
     return commands.check(wrapper)
