108811948305058974613cbe0b5ac6b2693432e9
f0xit <m.loe@gmx.de>
2023-02-03 05:04:38 +0100

    small refactoring and docstrings for event module

--- event.py
+++ event.py
@@ -1,9 +1,13 @@
+'''This module contains the event class'''
 import logging
-import tools.json_tools as json_tools
 from discord import User, Member
+from tools import json_tools
 
 
 class Event:
+    """This class is used for events like streams or coop sessions
+    """
+
     def __init__(self, event_type: str) -> None:
         self.event_type = event_type
         self.event_time = ''
@@ -13,17 +17,32 @@ class Event:
         self.load()
 
     def update_event(self, event_time: str, event_game: str) -> None:
+        """Updates the event.
+
+        Args:
+            event_time (str): Format is HH:MM
+            event_game (str): Name of the game played/streamed
+        """
         self.event_time = event_time
         self.event_game = event_game
 
         self.save()
 
     def add_member(self, new_member: User | Member) -> None:
-        if new_member.id not in self.event_members.keys():
-            self.event_members[new_member.id] = new_member.display_name
-            self.save()
+        """Adds a member to the event
+
+        Args:
+            new_member (User | Member): The added member
+        """
+        if new_member.id in self.event_members.keys():
+            return
+
+        self.event_members[new_member.id] = new_member.display_name
+        self.save()
 
     def reset(self) -> None:
+        """Resets all instance attributes
+        """
         self.event_time = ''
         self.event_game = ''
         self.event_members = {}
@@ -31,6 +50,8 @@ class Event:
         self.save()
 
     def save(self) -> None:
+        """Saves the event to a json-file
+        """
         if json_tools.save_file(self.event_type + '.json', self.__dict__):
             logging.info(
                 'Event saved. Type: %s - Name: %s - Time: %s - Members: %s',
@@ -49,19 +70,22 @@ class Event:
             )
 
     def load(self) -> None:
-        if (data := json_tools.load_file(self.event_type + '.json')) != None:
-            self.event_time = data['event_time']
-            self.event_game = data['event_game']
-            self.event_members = data['event_members']
-
-            logging.info(
-                'Event loaded. Type: %s - Name: %s - Time: %s - Members: %s',
-                self.event_type,
-                self.event_game,
-                self.event_time,
-                '.'.join(self.event_members.values())
-            )
-        else:
+        """Loads the event from a json-file if possible
+        """
+        if (data := json_tools.load_file(self.event_type + '.json')) is None:
             logging.error(
                 'Event could not be loaded!'
             )
+            return
+
+        self.event_time = data['event_time']
+        self.event_game = data['event_game']
+        self.event_members = data['event_members']
+
+        logging.info(
+            'Event loaded. Type: %s - Name: %s - Time: %s - Members: %s',
+            self.event_type,
+            self.event_game,
+            self.event_time,
+            '.'.join(self.event_members.values())
+        )
