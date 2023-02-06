3559cd83f1149af46dc7af437c4efd3de36f91b0
f0xit <m.loe@gmx.de>
2023-02-06 04:17:59 +0100

    documentation and refactor for gartic cog removed the pick command refactored the generation of a random painting and moved it to seperate function

--- cogs/gartic.py
+++ cogs/gartic.py
@@ -1,3 +1,4 @@
+'''Cog for the faith point mechanic'''
 import os
 import re
 import random
@@ -12,11 +13,53 @@ from tools.dt_tools import get_local_timezone
 
 
 async def setup(bot: Bot) -> None:
+    '''Setup function for the cog.'''
     await bot.add_cog(Gartic(bot))
-    logging.info("Cog: Gartic geladen.")
+    logging.info('Cog: Gartic geladen.')
+
+
+async def generate_random_painting() -> None:
+    '''Generates a combination of prompt and drawing from a random gartic phone game.'''
+
+    gartic_rounds = [
+        round for round in os.listdir("gartic")
+        if re.match(r"^\d{3}$", round)
+    ]
+
+    gartic_round = random.choice(gartic_rounds)
+
+    gartic_stories = [
+        story for story in os.listdir(f"gartic/{gartic_round}")
+        if re.match(r"^album_.*\.gif$", story)
+    ]
+
+    gartic_story = random.choice(gartic_stories)
+
+    story_gif = Image.open(f"gartic/{gartic_round}/{gartic_story}")
+
+    position = random.randint(0, math.floor(story_gif.n_frames/2) - 1)
+
+    story_gif.seek(2*position)
+    story_gif.save("gartic_text.png")
+    story_gif.seek(2*position + 1)
+    story_gif.save("gartic_image.png")
+
+    output_image = Image.new(
+        'RGB', (story_gif.width, 2*story_gif.height)
+    )
+
+    image_top = Image.open("gartic_text.png")
+    image_bottom = Image.open("gartic_image.png")
+
+    output_image.paste(im=image_top, box=(0, 0))
+    output_image.paste(im=image_bottom, box=(0, story_gif.height))
+
+    output_image.save("gartic_output.png")
 
 
 class Gartic(commands.Cog, name='Gartic'):
+    '''This cog includes everything related to the gartic mechanic'''
+
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.daily_gartic.start()
@@ -28,94 +71,11 @@ class Gartic(commands.Cog, name='Gartic'):
         name='gartic',
         brief='Zeigt ein zufälliges Gartic-Gemälde aus dem Archiv.'
     )
-    async def generate_random_painting(
+    async def _gartic(
         self,
-        ctx: commands.Context,
-        channel: discord.TextChannel = None
+        ctx: commands.Context
     ) -> None:
-        try:
-            raw_rounds = os.listdir("gartic")
-            rounds = []
-
-            for round in raw_rounds:
-                if re.match(r"^\d{3}$", round):
-                    rounds.append(round)
-
-            round = random.choice(rounds)
-        except Exception as e:
-            logging.error(e)
-
-        try:
-            raw_stories = os.listdir(f"gartic/{round}")
-            stories = []
-
-            for story in raw_stories:
-                if re.match(r"^album_.*\.gif$", story):
-                    stories.append(story)
-
-            story = random.choice(stories)
-        except Exception as e:
-            logging.error(e)
-
-        try:
-            story_gif = Image.open(f"gartic/{round}/{story}")
-
-            position = random.randint(0, math.floor(story_gif.n_frames/2) - 1)
-        except Exception as e:
-            logging.error(e)
-
-        try:
-            story_gif.seek(2*position)
-            story_gif.save("gartic_text.png")
-            story_gif.seek(2*position + 1)
-            story_gif.save("gartic_image.png")
-
-            output_image = Image.new(
-                'RGB', (story_gif.width, 2*story_gif.height))
-
-            image_top = Image.open("gartic_text.png")
-            image_bottom = Image.open("gartic_image.png")
-
-            output_image.paste(im=image_top, box=(0, 0))
-            output_image.paste(im=image_bottom, box=(0, story_gif.height))
-
-            output_image.save("gartic_output.png")
-
-            if channel is None:
-                await ctx.send(
-                    file=discord.File('gartic_output.png')
-                )
-            else:
-                await channel.send(
-                    "Guten Abend, Krah Krah! Hier kommt das tägliche "
-                    + "Highlight aus dem Gartic Phone-Archiv, Krah Krah!",
-                    file=discord.File('gartic_output.png')
-                )
-
-        except Exception as e:
-            logging.error(e)
-
-    @commands.command(
-        name='pick',
-        brief='Zeigt ein spezifisches Gartic-Gemälde aus dem Archiv.'
-    )
-    async def pick(
-        self,
-        ctx: commands.Context,
-        round: str,
-        album: int,
-        position: int
-    ) -> None:
-        raw_stories = os.listdir(f"gartic/{round}")
-        stories = []
-
-        for story in raw_stories:
-            if re.match(r"^album_.*\.gif$", story):
-                stories.append(story)
-
-        output_image = Image.open(f"gartic/{round}/{stories[int(album)]}")
-        output_image.seek(2*int(position)+1)
-        output_image.save("gartic_output.png")
+        generate_random_painting()
 
         await ctx.send(
             file=discord.File('gartic_output.png')
@@ -123,16 +83,16 @@ class Gartic(commands.Cog, name='Gartic'):
 
     @tasks.loop(time=dt.time(19, 30, tzinfo=get_local_timezone()))
     async def daily_gartic(self) -> None:
-        gartic_channel = 815702384688234538
-        try:
-            await self.generate_random_painting(
-                None,
-                channel=self.bot.get_channel(gartic_channel)
-            )
-        except Exception as exc_msg:
-            logging.error(
-                'ERROR: Kein Gartic-Image des Tages: %s', exc_msg
-            )
+        '''Posts a daily gartic image to the right channel.'''
+        generate_random_painting()
+
+        channel = self.bot.get_channel(815702384688234538)
+
+        await channel.send(
+            "Guten Abend, Krah Krah! Hier kommt das tägliche "
+            + "Highlight aus dem Gartic Phone-Archiv, Krah Krah!",
+            file=discord.File('gartic_output.png')
+        )
 
     @daily_gartic.before_loop
     async def _before_gartic_loop(self):
