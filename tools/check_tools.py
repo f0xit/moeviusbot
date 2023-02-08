b713bcaeb98cd27a6b8a9a6c188ed7b15181b379
f0xit <m.loe@gmx.de>
2023-02-08 03:46:34 +0100

    fixed typing in check_tools

--- tools/check_tools.py
+++ tools/check_tools.py
@@ -1,10 +1,13 @@
 from discord.ext import commands
-from tools import json_tools
+from json_tools import load_file
 
 
 def is_super_user():
-    async def wrapper(ctx: commands.Context):
-        settings = json_tools.load_file('json/settings.json')
-        # Request commands should be independent from su-rank
+    async def wrapper(ctx: commands.Context) -> bool:
+        settings = load_file('json/settings.json')
+
+        if settings is None:
+            return False
+
         return ctx.author.name in settings['super-users']
     return commands.check(wrapper)
