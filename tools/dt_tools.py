2c783913884ad12483d242e25b6fc76aa95e30c2
f0xit <m.loe@gmx.de>
2023-02-08 03:44:48 +0100

    fixed return type for get_local_timezone

--- tools/dt_tools.py
+++ tools/dt_tools.py
@@ -1,10 +1,10 @@
 import datetime as dt
 
 
-def get_local_timezone() -> dt.tzinfo:
+def get_local_timezone() -> dt.tzinfo | None:
     """Quick tool to get the local timezone.
 
     Returns:
-        dt._TzInfo: Timezone object with the local timezone
-    """
+        dt._TzInfo: Timezone object with the local timezone"""
+
     return dt.datetime.now().astimezone().tzinfo
