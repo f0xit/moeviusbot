eb267fc31b8995df8ff73a72c94b943f0460cf9a
f0xit <m.loe@gmx.de>
2023-11-10 03:25:14 +0100

    rewrote event_tools to work with orm sql

--- tools/event_tools.py
+++ tools/event_tools.py
@@ -1,92 +1,50 @@
 """This module contains the classes and functions for handling events in the reminder part
 of the discord bot"""
 
+from __future__ import annotations
+
 import datetime as dt
-from dataclasses import dataclass, field
-from enum import Enum
+from enum import Enum, auto
+from typing import Optional
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from tools.db_tools import Base
+
+DEFAULT_TIME_FMT = "%d.%m um %H:%M Uhr"
 
 
 class EventType(Enum):
     """Enum of the supported event types"""
 
-    GAME = "Game"
-    STREAM = "Stream"
+    GAME = auto()
+    STREAM = auto()
 
 
-@dataclass
-class Event:
-    """Datalass to represent events like streams and games"""
+class Event(Base):
+    """This class is used for events like streams or coop sessions"""
 
-    event_title: str
-    event_type: EventType
-    event_dt: dt.datetime
-    event_id: int = field(default=0, init=False)
-    event_game: str = field(default="")
-    event_members: list[int] = field(default_factory=list[int], init=False)
+    __tablename__ = "events"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[EventType]
+    title: Mapped[Optional[str]]
+    description: Mapped[Optional[str]]
+    time: Mapped[dt.datetime]
+    creator: Mapped[int]
+    announced: Mapped[bool] = mapped_column(default=False)
+    started: Mapped[bool] = mapped_column(default=False)
 
     def __repr__(self) -> str:
-        return str(self.__dict__)
+        return (
+            f"Event(id={self.id!r}, type={self.type!r}, title={self.title!r}, "
+            f"description={self.description!r}, time={self.time!r}, creator={self.creator!r}, "
+            f"announced={self.announced!r}, started={self.started!r})"
+        )
+
+    def __str__(self) -> str:
+        return f"#{self.id:04}-{self.type}: {self.time} {self.title}"
 
     @property
-    def is_past(self) -> bool:
-        """Is True if the event is in the past"""
-        return self.event_dt < dt.datetime.now()
-
-
-class EventList(list[Event]):
-    """Class to orgaize events in lists"""
-
-    @property
-    def max_event_id(self) -> int:
-        """Returns the highest event id in the event list"""
-        if not self:
-            return 0
-
-        return max(event.event_id for event in self)
-
-
-@dataclass
-class EventManager:
-    """Class to manage event lists"""
-
-    upcoming_events: EventList = field(default_factory=EventList)
-    past_events: EventList = field(default_factory=EventList)
-
-    @property
-    def max_event_id(self) -> int:
-        """Returns the highest event id in the event lists of past and upcoming events"""
-
-        return max(self.upcoming_events.max_event_id, self.past_events.max_event_id)
-
-    def add_event(self, event: Event) -> None:
-        """Adds an event to the corresponding list"""
-
-        event.event_id = self.max_event_id + 1
-
-        if event.is_past:
-            self.past_events.append(event)
-        else:
-            self.upcoming_events.append(event)
-
-
-event_manager = EventManager()
-
-event_manager.add_event(
-    Event(
-        "Spaß mit Schnenk",
-        EventType.STREAM,
-        dt.datetime.fromordinal(1),
-    )
-)
-
-event_manager.add_event(
-    Event(
-        "Spaß mit Hans",
-        EventType.STREAM,
-        dt.datetime.fromordinal(1),
-    )
-)
-
-event_manager.add_event(
-    Event("Spaß mit OW", EventType.GAME, dt.datetime.fromisocalendar(2023, 20, 3))
-)
+    def fmt_dt(self) -> str:
+        return self.time.strftime(DEFAULT_TIME_FMT)
