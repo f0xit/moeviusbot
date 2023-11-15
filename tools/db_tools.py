170ae1787878cdf65e0322e43cb53ff6be68f27a
f0xit <m.loe@gmx.de>
2023-11-15 03:10:47 +0100

    Added logging to db_tools

--- tools/db_tools.py
+++ tools/db_tools.py
@@ -1,3 +1,5 @@
+import logging
+
 import sqlalchemy
 from sqlalchemy.orm import DeclarativeBase
 
@@ -10,9 +12,12 @@ async def create_all(engine: sqlalchemy.Engine) -> None:
     """This function creates all required tables in the DB."""
 
     Base.metadata.create_all(engine)
+    logging.debug("All tables in DB created.")
 
 
 def create_engine(db_url: str = "sqlite:///storage.db", echo: bool = False) -> sqlalchemy.Engine:
     """This function creates and returns a DB engine."""
 
-    return sqlalchemy.create_engine(db_url, echo=echo)
+    engine = sqlalchemy.create_engine(db_url, echo=echo)
+    logging.debug("DB engine created.")
+    return engine
