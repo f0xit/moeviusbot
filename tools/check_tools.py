7e37764a96766b144e11be5f7f9c775aa301a7cf
f0xit <m.loe@gmx.de>
2024-03-15 04:11:45 +0100

    added check for special users

--- tools/check_tools.py
+++ tools/check_tools.py
@@ -1,6 +1,23 @@
+from enum import Enum
+
 from discord.ext import commands
 
 from tools.json_tools import load_file
+
+
+class SpecialUser(Enum):
+    """Enum of special users."""
+
+    HANS = 247117682875432960
+    SCHNENK = 257249704872509441
+    ZUGGI = 232561052573892608
+
+
+def is_special_user(user_list: list[SpecialUser]):
+    async def wrapper(ctx: commands.Context) -> bool:
+        return ctx.author.id in [user.value for user in user_list]
+
+    return commands.check(wrapper)
 
 
 def is_super_user():
