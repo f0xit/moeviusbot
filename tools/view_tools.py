14d1690fc101c989cd2d0004f733c7f88cbdd082
f0xit <m.loe@gmx.de>
2024-04-15 16:35:07 +0200

    Docstrings and typing in view tools

--- tools/view_tools.py
+++ tools/view_tools.py
@@ -1,23 +1,39 @@
+"""This tool contains functions an classes that help working with discord views."""
+
 from __future__ import annotations
+
+from typing import Any
 
 import discord
 
+Choice = tuple[str, str]
+Choices = list[Choice]
+
 
 def emoji_from_asciilo(ch: str) -> str:
+    """Transforms lowercase ascii chars into corresponding regional indicator."""
     return chr(0x1F1E6 + ord(ch) - 97)
 
 
 class PollView(discord.ui.View):
+    """Represents a special discord view that contains buttons for polls."""
+
     def __init__(self):
         super().__init__(timeout=None)
 
-    def buttons_from_choices(self, new_poll_id, choices) -> PollView:
+    def buttons_from_choices(self, new_poll_id: str, choices: Choices) -> PollView:
+        """Populates the view with buttons from a list of choices. Returns itself for daisy chaining."""
+
         for choice in choices:
             self.add_item(PollButton(choice, new_poll_id))
 
         return self
 
-    def buttons_from_collection(self, choices, votes, poll_id, iteration) -> PollView:
+    def buttons_from_collection(
+        self, choices: dict[str, str], votes: dict[str, list], poll_id: str, iteration: Any
+    ) -> PollView:
+        """Populates the view with buttons from a collection. Returns itself for daisy chaining."""
+
         for choice in choices.items():
             vote_count = len([item for row in votes.values() for item in row if item == choice[0]])
 
@@ -27,7 +43,9 @@ class PollView(discord.ui.View):
 
         return self
 
-    def deactivate_buttons_from_collection(self, choices, votes) -> PollView:
+    def deactivate_buttons_from_collection(self, choices: dict[str, str], votes: dict[str, list]) -> PollView:
+        """Populates the view with deactivated buttons. Returns itself for daisy chaining."""
+
         for choice in choices.items():
             vote_count = len([item for row in votes.values() for item in row if item == choice[0]])
 
@@ -37,6 +55,8 @@ class PollView(discord.ui.View):
 
 
 class PollButton(discord.ui.Button):
+    """Represents special discord buttons that are used in polls."""
+
     def __init__(
         self,
         choice: tuple[str, str],
@@ -55,6 +75,8 @@ class PollButton(discord.ui.Button):
 
 
 class InactivePollButton(discord.ui.Button):
+    """Represents special discord buttons that are used in polls. Inactive by design."""
+
     def __init__(
         self,
         choice: tuple[str, str],
