5f730662b08e501adbccaa89052445fa1c53d5d6
f0xit <m.loe@gmx.de>
2024-07-16 03:54:48 +0200

    now escaping markdown in embeds

--- tools/embed_tools.py
+++ tools/embed_tools.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import discord
+from discord.utils import escape_markdown
 
 from tools.dt_tools import get_random_date
 
@@ -11,9 +12,9 @@ class PollEmbed(discord.Embed):
     def __init__(self, poll_id: str, poll: dict[str, Any]):
         super().__init__(
             colour=MOEVIUS_COLOR,
-            title=poll["title"],
+            title=escape_markdown(poll["title"]),
             type="rich",
-            description=poll["description"],
+            description=escape_markdown(poll["description"]),
         )
 
         self.set_footer(text=f"Umfrage-ID: {poll_id}")
@@ -23,9 +24,9 @@ class QuoteEmbed(discord.Embed):
     def __init__(self, title: str, quote: str, quote_by: str):
         super().__init__(
             colour=MOEVIUS_COLOR,
-            title=title,
+            title=escape_markdown(title),
             type="rich",
-            description=quote,
+            description=escape_markdown(quote),
             timestamp=get_random_date(),
         )
 
