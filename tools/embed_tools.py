782ec0728ca6bce79433e5a8e682248fd79e60c9
f0xit <m.loe@gmx.de>
2024-07-17 18:44:36 +0200

    fixed bug in poll embed description and title are now only displayed if needed

--- tools/embed_tools.py
+++ tools/embed_tools.py
@@ -10,12 +10,13 @@ MOEVIUS_COLOR = 0xFF06B5
 
 class PollEmbed(discord.Embed):
     def __init__(self, poll_id: str, poll: dict[str, Any]):
-        super().__init__(
-            colour=MOEVIUS_COLOR,
-            title=escape_markdown(str(poll["title"])),
-            type="rich",
-            description=escape_markdown(str(poll["description"])),
-        )
+        super().__init__(colour=MOEVIUS_COLOR, type="rich")
+
+        if isinstance(poll["title"], str):
+            self.title = escape_markdown(poll["title"])
+
+        if isinstance(poll["description"], str):
+            self.description = escape_markdown(poll["description"])
 
         self.set_footer(text=f"Umfrage-ID: {poll_id}")
 
@@ -24,9 +25,9 @@ class QuoteEmbed(discord.Embed):
     def __init__(self, title: str, quote: str, quote_by: str):
         super().__init__(
             colour=MOEVIUS_COLOR,
-            title=escape_markdown(str(title)),
+            title=title,
             type="rich",
-            description=escape_markdown(str(quote)),
+            description=quote,
             timestamp=get_random_date(),
         )
 
