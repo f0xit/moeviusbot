8221b172e2f292931a39563f112277402bdfa4b3
f0xit <m.loe@gmx.de>
2024-10-10 13:17:24 +0200

    renaming and restructuring polls cog renamed variables for poll command moved poll logic into own sub-class for better readability

--- cogs/polls.py
+++ cogs/polls.py
@@ -27,6 +27,21 @@ async def setup(bot: Bot) -> None:
     logging.info("Cog loaded: Polls.")
 
 
+class Poll(discord.Poll):
+    def __init__(
+        self,
+        question: str,
+        duration: dt.timedelta,
+        choices: list[tuple[str, str]],
+        *,
+        multiple: bool,
+    ) -> None:
+        super().__init__(escape_markdown(question), duration, multiple=multiple)
+
+        for ch, text in choices:
+            self.add_answer(text=text, emoji=emoji_from_asciilo(ch))
+
+
 class Polls(commands.Cog, name="Umfragen"):
     """This cog includes commands for building polls"""
 
@@ -39,67 +54,53 @@ class Polls(commands.Cog, name="Umfragen"):
     @is_special_user([SpecialUser.SCHNENK, SpecialUser.HANS, SpecialUser.ZUGGI])
     @commands.hybrid_group(name="poll", fallback="start")
     @discord.app_commands.rename(
-        title="titel",
-        description="beschreibung",
+        question="frage",
         choices_str="antworten",
-        ends_at_str="endzeitpunkt",
-        multiple="mehrfachantwort",
+        description="beschreibung",
+        end_str="endzeitpunkt",
+        multi_str="mehrfachantwort",
     )
     @discord.app_commands.describe(
-        title="Titel der Umfrage",
+        question="Was möchtest du fragen?",
         choices_str="Antwortmöglichkeiten, getrennt mit Semikolon",
         description="Optionale Beschreibung",
-        ends_at_str="HH:MM oder DD.MM. HH:MM",
-        multiple="Ja oder Nein? Standard: Ja",
+        end_str="HH:MM oder DD.MM. HH:MM",
+        multi_str="Standard: Ja",
     )
     async def _poll(
         self,
         ctx: commands.Context,
-        title: str,
+        question: str,
         choices_str: str,
         description: Optional[str],
-        ends_at_str: Optional[str],
-        multiple: Optional[Literal["Ja", "Nein"]],
+        end_str: Optional[str],
+        multi_str: Optional[Literal["Ja", "Nein"]],
     ) -> None:
         await ctx.defer()
 
-        if ends_at_str is None:
-            timedelta_to_end = dt.timedelta(weeks=2)
-        else:
-            try:
-                end_date = await convert_str_to_dt(ends_at_str)
-                timedelta_to_end = end_date - dt.datetime.now(get_local_timezone())
-            except BadArgument:
-                await ctx.send(
-                    "Datum nicht erkannt. Bitte verwende HH:MM oder DD.MM. HH:MM, Krah Krah!",
-                    ephemeral=True,
-                )
-                return
-
-        multi_choice = multiple is None or multiple == "Ja"
-
-        new_poll = discord.Poll(
-            escape_markdown(title),
-            duration=timedelta_to_end,
-            multiple=multi_choice,
-        )
-
         try:
-            for ch, text in convert_choices_to_list(choices_str):
-                new_poll.add_answer(text=text, emoji=emoji_from_asciilo(ch))
+            if end_str is None:
+                duration = dt.timedelta(weeks=2)
+            else:
+                duration = await convert_str_to_dt(end_str) - dt.datetime.now(get_local_timezone())
         except BadArgument:
-            await ctx.send(
-                "Bitte gib mindestens 2 Antwortmöglichkeiten an. Beispiel: Apfel; Birne; Krah Krah!",
-                ephemeral=True,
-            )
+            await ctx.send("Datum nicht erkannt. Bitte verwende HH:MM oder DD.MM. HH:MM, Krah Krah!", ephemeral=True)
             return
 
-        await ctx.send(description, poll=new_poll)
+        multiple = multi_str is None or multi_str == "Ja"
+
+        try:
+            choices = convert_choices_to_list(choices_str)
+        except BadArgument:
+            await ctx.send("Bitte gib mindestens 2 Antwortmöglichkeiten an, Krah Krah!", ephemeral=True)
+            return
+
+        await ctx.send(description, poll=Poll(question, duration, choices, multiple=multiple))
 
     @is_special_user([SpecialUser.SCHNENK, SpecialUser.HANS, SpecialUser.ZUGGI])
     @_poll.command(name="stop")
     @discord.app_commands.rename(msg="nachricht")
-    @discord.app_commands.describe(msg="Nachricht zur Umfrage")
+    @discord.app_commands.describe(msg="Nachricht mit der Umfrage")
     async def _poll_stop(self, ctx: commands.Context, msg: discord.Message) -> None:
         await msg.end_poll()
 
