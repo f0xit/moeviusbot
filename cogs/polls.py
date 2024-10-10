597b4dd140dacdf4b7b0775c01d71280aafcdaf9
f0xit <m.loe@gmx.de>
2024-10-10 13:40:35 +0200

    outscoursed cmd info into dict may be moved to a json file later

--- cogs/polls.py
+++ cogs/polls.py
@@ -14,6 +14,34 @@ from tools.check_tools import SpecialUser, is_special_user
 from tools.converter_tools import convert_choices_to_list, convert_str_to_dt
 from tools.dt_tools import get_local_timezone
 
+cog_info = {
+    "cog": {"name": "Umfragen"},
+    "poll": {
+        "start": {
+            "cmd": {"name": "poll", "fallback": "start"},
+            "rename": {
+                "question": "frage",
+                "choices_str": "antworten",
+                "description": "beschreibung",
+                "end_str": "endzeitpunkt",
+                "multi_str": "mehrfachantwort",
+            },
+            "describe": {
+                "question": "Was möchtest du fragen?",
+                "choices_str": "Antwortmöglichkeiten, getrennt mit Semikolon",
+                "description": "Optionale Beschreibung",
+                "end_str": "HH:MM oder DD.MM. HH:MM",
+                "multi_str": "Standard: Ja",
+            },
+        },
+        "stop": {
+            "cmd": {"name": "stop"},
+            "rename": {"msg": "nachricht"},
+            "describe": {"msg": "Nachricht mit der Umfrage"},
+        },
+    },
+}
+
 
 def emoji_from_asciilo(ch: str) -> str:
     """Transforms lowercase ascii chars into corresponding regional indicator."""
@@ -29,12 +57,7 @@ async def setup(bot: Bot) -> None:
 
 class Poll(discord.Poll):
     def __init__(
-        self,
-        question: str,
-        duration: dt.timedelta,
-        choices: list[tuple[str, str]],
-        *,
-        multiple: bool,
+        self, question: str, duration: dt.timedelta, choices: list[tuple[str, str]], *, multiple: bool
     ) -> None:
         super().__init__(escape_markdown(question), duration, multiple=multiple)
 
@@ -42,7 +65,7 @@ class Poll(discord.Poll):
             self.add_answer(text=text, emoji=emoji_from_asciilo(ch))
 
 
-class Polls(commands.Cog, name="Umfragen"):
+class Polls(commands.Cog, **cog_info["cog"]):
     """This cog includes commands for building polls"""
 
     def __init__(self, bot: Bot):
@@ -52,21 +75,9 @@ class Polls(commands.Cog, name="Umfragen"):
         logging.info("Cog unloaded: Polls.")
 
     @is_special_user([SpecialUser.SCHNENK, SpecialUser.HANS, SpecialUser.ZUGGI])
-    @commands.hybrid_group(name="poll", fallback="start")
-    @discord.app_commands.rename(
-        question="frage",
-        choices_str="antworten",
-        description="beschreibung",
-        end_str="endzeitpunkt",
-        multi_str="mehrfachantwort",
-    )
-    @discord.app_commands.describe(
-        question="Was möchtest du fragen?",
-        choices_str="Antwortmöglichkeiten, getrennt mit Semikolon",
-        description="Optionale Beschreibung",
-        end_str="HH:MM oder DD.MM. HH:MM",
-        multi_str="Standard: Ja",
-    )
+    @commands.hybrid_group(**cog_info["poll"]["start"]["cmd"])
+    @discord.app_commands.rename(**cog_info["poll"]["start"]["rename"])
+    @discord.app_commands.describe(**cog_info["poll"]["start"]["describe"])
     async def _poll(
         self,
         ctx: commands.Context,
@@ -98,10 +109,9 @@ class Polls(commands.Cog, name="Umfragen"):
         await ctx.send(description, poll=Poll(question, duration, choices, multiple=multiple))
 
     @is_special_user([SpecialUser.SCHNENK, SpecialUser.HANS, SpecialUser.ZUGGI])
-    @_poll.command(name="stop")
-    @discord.app_commands.rename(msg="nachricht")
-    @discord.app_commands.describe(msg="Nachricht mit der Umfrage")
+    @_poll.command(**cog_info["poll"]["stop"]["cmd"])
+    @discord.app_commands.rename(**cog_info["poll"]["stop"]["rename"])
+    @discord.app_commands.describe(**cog_info["poll"]["stop"]["describe"])
     async def _poll_stop(self, ctx: commands.Context, msg: discord.Message) -> None:
         await msg.end_poll()
-
         await ctx.send("Umfrage wurde gestoppt, Krah Krah!", ephemeral=True)
